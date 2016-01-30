044eb9cae57eaf65c7f9e929524f93f059fd4c16
william <willthingy@hotmail.com>
2016-01-29 22:18:30 -0500

    first project commit, has buyoancy

---
+++ Assets/Alphaghetti.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Alphaghetti : MonoBehaviour {
+
+	public float Buoyancy=1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(transform.position.y<10){
+			Buoy ();
+		}
+	}
+
+	void Buoy(){
+		GetComponent<Rigidbody>().AddForce(new Vector3(0,Buoyancy*Time.deltaTime,0));
+	}
+}
