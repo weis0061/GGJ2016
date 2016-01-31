c1521fb2630bbe49e038db89afdc18b905aa6293
weis0061 <willthingy@hotmail.com>
2016-01-30 21:47:40 -0500

    words can now be created.

--- Assets/Alphaghetti.cs
+++ Assets/Alphaghetti.cs
@@ -8,16 +8,25 @@ public class Alphaghetti : MonoBehaviour
     public float Torque= 100; 
     Rigidbody rb;
     public char MyChar;
+    bool IsRemoved;
     // Use this for initialization
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        IsRemoved = false;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        //if (transform.position.y < 10)
+        if (IsRemoved)
+        {
+            rb.AddForce(Vector3.up * Buoyancy*Time.deltaTime);
+            if (transform.position.y > 50) 
+                Destroy(this.gameObject);
+            return;
+        }
+        if (transform.position.y < 10)
         {
             Buoy();
         }
@@ -25,38 +34,20 @@ public class Alphaghetti : MonoBehaviour
 
     void Buoy()
     {
-
-       // rb.AddForce(new Vector3(0, Buoyancy * Time.deltaTime, 0));
+        rb.AddForce(new Vector3(0, Buoyancy * Time.deltaTime, 0));
         if (Vector3.Dot(transform.rotation.eulerAngles, Vector3.up) >= 0)
         { }
             Vector3 TargetAngle=transform.rotation.eulerAngles;
-            //TargetAngle.x=0;
-            //TargetAngle = Vector3.zero;
-            //Vector3 VecDiff = TargetAngle - transform.rotation.eulerAngles;
-            //transform.Rotate(VecDiff * Time.deltaTime);
-            //VecDiff=transform.TransformDirection(VecDiff);
+            TargetAngle.x=0;
+            TargetAngle = Vector3.zero;
+            Vector3 VecDiff = TargetAngle - transform.rotation.eulerAngles;
+            transform.Rotate(VecDiff * Time.deltaTime);
+            VecDiff=transform.TransformDirection(VecDiff);
+    }
 
-            //if (TargetAngle.x < 0 )
-            //{
-            //    Vector3 VecDiff = new Vector3(1, 0, 0);
-            //    rb.AddTorque(VecDiff * Time.deltaTime * Torque,ForceMode.Force);
-            //}
-            //else
-            //{
-            //    Vector3 VecDiff = new Vector3(-1, 0, 0);
-            //    rb.AddTorque(VecDiff * Time.deltaTime * Torque, ForceMode.Force);
-            //}
-
-            if (TargetAngle.x < 90)
-            {
-                Vector3 VecDiff = new Vector3(1, 0, 0);
-                rb.AddTorque(VecDiff * Time.deltaTime * Torque, ForceMode.Force);
-            }
-            else
-            {
-                
-                Vector3 VecDiff = new Vector3(-1, 0, 0); 
-                rb.AddTorque(VecDiff * Time.deltaTime * Torque, ForceMode.Force);
-            }
+    public void RemoveFromBowl()
+    {
+        gameObject.layer = 0;
+        IsRemoved = true;
     }
 }
--- Assets/WordDetector.cs
+++ Assets/WordDetector.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 
 public class WordDetector : MonoBehaviour {
     List<GameObject> CurrentPastaList;
-    List<GameObject> ChainedPastaList;
     [System.Serializable]
     public struct Word
     {
@@ -15,52 +14,91 @@ public class WordDetector : MonoBehaviour {
     public GameObject[] AlphaghettiDatabase;
     public float AnglePerPasta;
     public float DistancePerPasta;
+    public Transform PutPastaPosition;
+    public float PutPastaRadius;
+    int WStart, WEnd;
+    Camera cam;
 	// Use this for initialization
 	void Start ()
     {
         CurrentPastaList = new List<GameObject>();
+        cam = GetComponent<Camera>();
+        WStart = 0; 
+        WEnd = 0;
+        AddPastas(20);
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-       // rcurf(CurrentPastaList[0],"");
-	}
+        Debug.Log("New Frame");
+        WStart = 0;
+        WEnd = 0;
+        for (int i = 0; i < CurrentPastaList.Count; i++)
+        {
+            rcurf(CurrentPastaList[i], "", new List<GameObject>());
+        }
+    }
 
     void AddPastas(int count)
     {
         for (int i = 0; i < count; i++)
         {
-            //CurrentPastaList.Add();
-            //GameObject go = GameObject.Instantiate(AlphaghettiDatabase[0]);
+            GameObject go = GameObject.Instantiate(AlphaghettiDatabase[0]);
+            go.transform.position = PutPastaPosition.position + Random.onUnitSphere * PutPastaRadius;
+            CurrentPastaList.Add(go);
         }
     }
 
-    void rcurf(GameObject go,string s)
+    void rcurf(GameObject go,string s,List<GameObject> Chain)
     {
-        if (!ChainedPastaList.Contains(go))
+        if (!Chain.Contains(go))
         {
+            Chain.Add(go);
+            int stringpos = s.Length;
+
             s = s + go.GetComponent<Alphaghetti>().MyChar;
-            RaycastHit[] rhits = Physics.SphereCastAll(go.transform.position, DistancePerPasta, go.transform.right, DistancePerPasta, LayerMask.NameToLayer("noodles"));
+            int mask = 1 << (int)LayerMask.NameToLayer("noodles");
+            RaycastHit[] rhits = Physics.RaycastAll(go.transform.position, cam.transform.right, DistancePerPasta,mask);
             for (int i = 0; i < rhits.Length; i++)
             {
-                if(Vector3.Angle(go.transform.right,rhits[i].transform.position-go.transform.position)<=AnglePerPasta)
-                rcurf(rhits[i].collider.gameObject, s);
+                Vector2 objPos= cam.WorldToScreenPoint(go.transform.position);
+                Vector2 newpos = cam.WorldToScreenPoint(rhits[i].transform.position);
+                float trigRatio = (newpos.x - objPos.x) / (newpos.y / objPos.y);
+                //if (Mathf.Tan(45) > trigRatio||Mathf.Tan(-45)<trigRatio)
+                {
+                    rcurf(rhits[i].collider.gameObject, s, Chain);
+                }
             }
             if (rhits.Length == 0)
             {
-                CheckDictionary(s);
+                CheckDictionary(s, out WStart, out WEnd);
+            }
+            if (WEnd != 0)
+            {
+                if (stringpos >= WStart && stringpos <= WEnd)
+                {
+                    go.GetComponent<Alphaghetti>().RemoveFromBowl();
+                    CurrentPastaList.Remove(go);
+                    AddPastas(1);
+                }
+                return;
             }
         }
     }
 
-    void CheckDictionary(string s)
+    void CheckDictionary(string s,out int WordStart, out int WordEnd)
     {
+        WordStart = 0; 
+        WordEnd = 0;
         for (int i = 0; i < wordlist.Length; ++i)
         {
-            if (wordlist[i].value.Contains(s))
+            if (s.Contains(wordlist[i].value))
             {
-                Debug.Log("Word!");
+                WordStart = s.IndexOf(wordlist[i].value);
+                WordEnd = WordStart + s.Length;
+                Debug.Log("Word: " + wordlist[i].value);
+                return;
             }
         }
     }
