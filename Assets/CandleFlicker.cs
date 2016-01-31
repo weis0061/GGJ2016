9da93aec2770e547a91553d6783c89205185fd77
william <willthingy@hotmail.com>
2016-01-31 09:42:02 -0500

    Added materials and candle flicker script

---
+++ Assets/CandleFlicker.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Light))]
+public class CandleFlicker : MonoBehaviour {
+	Light l;
+	public float Min;
+	public float Max;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		l=GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		l.intensity=Mathf.Lerp(l.intensity,Random.Range(Min,Max),speed*Time.deltaTime);
+		//l.intensity+=(Random.Range(Min,Max)-Min)*speed;
+	}
+}
